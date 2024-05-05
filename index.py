ff7f6a845411071ec2d1cdeb1b86fc23b29e4a4c
DevXAni <anirbanbanerjee3112@gmail.com>
2024-05-06 00:53:40 +0530

    Creation: A random address generator using python[FLask]

---
+++ index.py
@@ -0,0 +1,76 @@
+from flask import Flask, jsonify
+import random_address
+
+
+state_full_names = {
+    "AL": "Alabama",
+    "AK": "Alaska",
+    "AZ": "Arizona",
+    "AR": "Arkansas",
+    "CA": "California",
+    "CO": "Colorado",
+    "CT": "Connecticut",
+    "DE": "Delaware",
+    "FL": "Florida",
+    "GA": "Georgia",
+    "HI": "Hawaii",
+    "ID": "Idaho",
+    "IL": "Illinois",
+    "IN": "Indiana",
+    "IA": "Iowa",
+    "KS": "Kansas",
+    "KY": "Kentucky",
+    "LA": "Louisiana",
+    "ME": "Maine",
+    "MD": "Maryland",
+    "MA": "Massachusetts",
+    "MI": "Michigan",
+    "MN": "Minnesota",
+    "MS": "Mississippi",
+    "MO": "Missouri",
+    "MT": "Montana",
+    "NE": "Nebraska",
+    "NV": "Nevada",
+    "NH": "New Hampshire",
+    "NJ": "New Jersey",
+    "NM": "New Mexico",
+    "NY": "New York",
+    "NC": "North Carolina",
+    "ND": "North Dakota",
+    "OH": "Ohio",
+    "OK": "Oklahoma",
+    "OR": "Oregon",
+    "PA": "Pennsylvania",
+    "RI": "Rhode Island",
+    "SC": "South Carolina",
+    "SD": "South Dakota",
+    "TN": "Tennessee",
+    "TX": "Texas",
+    "UT": "Utah",
+    "DC": "Washington",
+    "VT": "Vermont",
+    "VA": "Virginia",
+    "WA": "Washington",
+    "WV": "West Virginia",
+    "WI": "Wisconsin",
+    "WY": "Wyoming",
+}
+
+app = Flask(__name__)
+
+@app.route('/random-address', methods=['GET'])
+def get_random_address():
+    raw_address = random_address.real_random_address()
+    state_abbreviation = raw_address.get("state")
+    state_full = state_full_names.get(state_abbreviation, "Unknown")
+    enriched_address = {
+        "StreetAddress": raw_address.get("address1"),
+        "city": raw_address.get("city"),
+        "state": state_full,
+        "StateFullName": state_abbreviation,
+        "postalCode": raw_address.get("postalCode"),
+    }
+    return jsonify(enriched_address)
+
+if __name__ == '__main__':
+    app.run(debug=True)
